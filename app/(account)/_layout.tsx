758bdf134a689f7ea9445e77830604e9606ed0e1
Yazan Al-Kiswani <yznkiswani@gmail.com>
2025-10-11 20:12:43 +0200

    fix(account): simplify stack layout

--- app/(account)/_layout.tsx
+++ app/(account)/_layout.tsx
@@ -8,12 +8,6 @@ export default function AccountLayout() {
         animation: "slide_from_right",
         animationDuration: 200,
       }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="profile" />
-      <Stack.Screen name="addresses" />
-      <Stack.Screen name="orders" />
-      <Stack.Screen name="checkout-test" />
-    </Stack>
+    />
   )
 }
